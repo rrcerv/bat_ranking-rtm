456866f585a903c30bbe638331063b69c9bf50b9
Rodrigo <rodrigo@addmind.me>
2024-06-27 14:11:11 -0300

    Add to admin

--- ranking_rtm/admin.py
+++ ranking_rtm/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import RankingGerentes, RankingVendedores, RankingRegionais
+from .models import RankingGerentes, RankingVendedores, RankingRegionais, RankingTerritorioRegional
 
 # Register your models here.
 admin.site.register(RankingVendedores)
 admin.site.register(RankingGerentes)
 admin.site.register(RankingRegionais)
+admin.site.register(RankingTerritorioRegional)
